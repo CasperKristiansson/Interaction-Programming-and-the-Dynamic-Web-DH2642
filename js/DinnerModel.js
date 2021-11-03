91b0494684f3151231324e4306d82b99d6bd0bcd
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2021-11-03 10:38:15 +0100

    Task 1 Done (TW1.1)

---
+++ js/DinnerModel.js
@@ -0,0 +1,11 @@
+class DinnerModel{
+    constructor(guests =2){
+        this.setNumberOfGuests(guests);}
+
+    setNumberOfGuests(x) {
+        if (x <= 0 || x % 1 != 0) {
+            throw new Error("Number of guests must be greater than 0 and be a integer");
+        }
+        this.numberOfGuests = x;
+    }
+}
