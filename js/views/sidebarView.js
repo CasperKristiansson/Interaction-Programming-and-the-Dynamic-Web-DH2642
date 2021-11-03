f9ef1a8838fa2d5d75e4c7f6fb6247cd6737d923
Casper Kristiansson <casper.kristiansson@yahoo.se>
2021-11-03 20:00:11 +0100

    Task 4 Done, (TW1.4)

--- js/views/sidebarView.js
+++ js/views/sidebarView.js
@@ -1,9 +1,18 @@
 function SidebarView(props) {
     return (
         <div>
+            <button disabled = { props.guests <= 1 } onClick = { event => props.setGuests(props.guests - 1)}> - </button>
+            <span title = "guests"> { props.guests }</span>
             <button onClick = { event => props.setGuests(props.guests + 1)}> + </button>
-            <span title="guests">{props.guests}</span>
-            <button disabled = {props.guests <= 1} onClick = { event => props.setGuests(props.guests - 1)}> - </button>
        </div>
     );
 }
+
+const VueSidebarLocalState= {
+    data() { return {number:2};}, 
+    render() {  
+        return <SidebarView guests={this.number} 
+                setGuests={guestsAmount => this.number = guestsAmount} 
+                />;
+    }
+};
